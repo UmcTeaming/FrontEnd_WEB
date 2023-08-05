14d12b8979528f535e97049808fe6c1507a7bd45
SuaJeong-winter <sua.jeong.developer@gmail.com>
2023-08-05 21:33:00 +0900

    메인 페이지 수정2

--- teaming/src/components/Home/Homeongoingproject.js
+++ teaming/src/components/Home/Homeongoingproject.js
@@ -7,7 +7,7 @@ export const Homeongoingproject = () => {
   return (
     <div className='OngoingProjApp'>
       <div className='OngoingProjtitle'>진행중인 프로젝트&#62;</div>
-      <div className='OngoingProjsubtitle'>프로젝트의 상태가 '진행중'인 프로젝트입니다</div>
+      <div className='OngoingProjsubtitle'>프로젝트의 상태가 '진행중인' 프로젝트입니다</div>
 
       <div className='OngoingProjContent'>
         <div className='content'>
