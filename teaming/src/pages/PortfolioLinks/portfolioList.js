70d4717ccd9ab043904ace888b01099edba72969
nellie29 <kny@swu.ac.kr>
2023-08-16 10:26:29 +0900

    포폴&진행중프젝 수정

--- teaming/src/pages/PortfolioLinks/portfolioList.js
+++ teaming/src/pages/PortfolioLinks/portfolioList.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import "./portfolioList.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
-import { faCircle } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { faBorderAll } from "@fortawesome/free-solid-svg-icons";
-import { faStar } from "@fortawesome/free-regular-svg-icons";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import PjBoxTemplate from '../../components/portfolioComponents/portfolioListBoxType/pjBoxes.js';
@@ -62,7 +55,7 @@ const  PortfolioList = () => {
           <div className="component">
             <div className="route">
               <span>
-                <Link to="/home">
+                <Link to="/">
                   <FontAwesomeIcon icon={faHouse} />
                 </Link>           
                 <FontAwesomeIcon icon={faChevronRight} />
@@ -71,7 +64,7 @@ const  PortfolioList = () => {
             </div>
 
             <div className="Name">
-                <br/><br/>
+                <br/>
                 <span className="userName">카리나님의 포트폴리오</span><br/>
                 <span className="ment">지금까지의 프로젝트를 한눈에 모아보세요!</span>
             </div>
@@ -83,32 +76,13 @@ const  PortfolioList = () => {
             <div className="list">
               <div className="howToView">
               
-                  {/* 
-                  onClick = {() => {setViewBox(false); {handleClick()};}}
-                              
-                  style={{ stroke: active ? "red" : "green"}}
-                  >          
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    // stroke={active ? "#FFD008" : "#FAFAFA80"}
-                    viewBox="0 0 21 21"
-                    stroke-width="2"
-                    width="21px"
-                    height="23px"
-                  >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
-              </button> */}
+                 
               <button className='lineBtn'>
               <ListBtn onClick={() => setViewBox(false)} isActive={!viewBox}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              viewBox="0 0 22 20"
+              viewBox="0 0 22z 20"
               stroke-width="2"
               class="w-6 h-6"
               width="17px"
@@ -121,24 +95,7 @@ const  PortfolioList = () => {
               />
             </svg>
           </ListBtn></button>
-              {/* 
-                  onClick={() => {setViewBox(true); {handleClick2()};}}
-                    style={{ fill: active2 ? "pink" : "blue"}}
-                    >                
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      width="20px"
-                      height="20px"
-                      // fill={active ? "#FFD008" : "#FAFAFA80"}
-                    >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.25 2A2.25 2.25 0 002 4.25v2.5A2.25 2.25 0 004.25 9h2.5A2.25 2.25 0 009 6.75v-2.5A2.25 2.25 0 006.75 2h-2.5zm0 9A2.25 2.25 0 002 13.25v2.5A2.25 2.25 0 004.25 18h2.5A2.25 2.25 0 009 15.75v-2.5A2.25 2.25 0 006.75 11h-2.5zm9-9A2.25 2.25 0 0011 4.25v2.5A2.25 2.25 0 0013.25 9h2.5A2.25 2.25 0 0018 6.75v-2.5A2.25 2.25 0 0015.75 2h-2.5zm0 9A2.25 2.25 0 0011 13.25v2.5A2.25 2.25 0 0013.25 18h2.5A2.25 2.25 0 0018 15.75v-2.5A2.25 2.25 0 0015.75 11h-2.5z"
-                      clipRule="evenodd"
-                    />
-                    </svg>
-              </button> */}
+              
               <button className='boxBtn'>
               <CardBtn onClick={() => setViewBox(true)} isActive={viewBox}>
             <svg
