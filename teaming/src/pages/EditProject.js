ad0856ae2a0b427d9cf0b454ddeaecffc494ea1e
juhui88 <sand8594@hufs.ac.kr>
2023-08-20 14:04:43 +0900

    Fix 홈 링크 수정

--- teaming/src/pages/EditProject.js
+++ teaming/src/pages/EditProject.js
@@ -6,7 +6,7 @@ import { cls } from "../libs/utils";
 import axios from "axios";
 import { useRecoilState } from "recoil";
 import { memberIdState } from "../components/atom";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const Span = tw.span`
 text-mainMoreDeepColor font-bold
@@ -248,20 +248,23 @@ const NewProject = () => {
       <form onSubmit={handleSubmit(onValid)} className="flex space-x-6">
         <div>
           <div className="flex text-mainDeepColor items-center space-x-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-5 h-5"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-              />
-            </svg>
+            <Link to="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
+            </Link>
+
             <span className="pt-1">&#62;프로젝트 수정하기</span>
           </div>
           <div className="mt-2">
