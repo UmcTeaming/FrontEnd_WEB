0c3f1984626e28a73350c603bb93417cd86883d5
nanna29 <luna2907@naver.com>
2023-09-03 17:30:27 +0900

    진행중&포폴 삭제 구현중

--- teaming/src/components/OngoingProjectComponents/OngoingProjectBoxType/opjBox.js
+++ teaming/src/components/OngoingProjectComponents/OngoingProjectBoxType/opjBox.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 import "../../../pages/OngoingProject/OngoingProject.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { FiTrash2 } from 'react-icons/fi';
+import styled from "styled-components";
+import axios from "axios";
+
+const Delete = styled.button`
+  margin-bottom:5px;
+  color: ${props => props.isDeleting ? "red" : "inherit"};
+  cursor: ${props => (props.isDeleting ? "not-allowed" : "pointer")}; // 커서 변경
+`;
 
 const OPjBox = ({ project }) => {
   const defaultImage = "/img/projectImg/project_img.jpg"; // 기본 이미지 경로 설정
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const onDelete = (e) => {
+    e.preventDefault();
+
+    if (
+      window.confirm(
+        "삭제한 프로젝트는 되돌릴 수 없습니다. 그래도 삭제하시겠습니까? "
+      )
+    ) {
+      console.log ("확인")
+      setIsDeleting(true); // 삭제 진행 중임을 표시
+      axios
+        .delete(
+          //`${process.env.REACT_APP_API_URL}/projects/${memberId}/${projectId}/files/${file.file_id}`
+        )
+        .then((response) => {
+          //console.log(response);
+          //if (matchProject) {
+            //queryClient.invalidateQueries("project-files");
+          //} else if (matchFinal) {
+            //queryClient.invalidateQueries("final-files");
+          //}
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        })
+        .finally(() => {
+          setIsDeleting(false); // 삭제 완료 또는 실패 시 상태 변경
+        });
+    }
+  };
+
+
   return (
     <Link to={`/${project.projectId}/project-files`}>
       <div className="box">
@@ -26,6 +69,9 @@ const OPjBox = ({ project }) => {
           <br />
           <p className="p">
             {project.projectStartDate}~{project.projectEndDate}
+            <Delete onClick={onDelete} isDeleting={isDeleting}>
+              <FiTrash2 size="19"/>
+            </Delete>
           </p>
         </div>
       </div>
--- teaming/src/components/portfolioComponents/portfolioListBoxType/pjBox.js
+++ teaming/src/components/portfolioComponents/portfolioListBoxType/pjBox.js
@@ -3,9 +3,44 @@ import "../../../pages/OngoingProject/OngoingProject.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { FiTrash2 } from 'react-icons/fi';
+import styled from "styled-components";
+import axios from "axios";
+
+const Delete = styled.button`
+  margin-bottom:5px;
+`;
 
 const PjBox = ({ project }) => {
     const defaultImage = "/img/projectImg/project_img.jpg"; // 기본 이미지 경로 설정
+
+    const onDelete = (e) => {
+        e.preventDefault();
+    
+        if (
+          window.confirm(
+            "삭제한 프로젝트는 되돌릴 수 없습니다. 그래도 삭제하시겠습니까? "
+          )
+        ) {
+          // axios
+          //   .delete(
+          //     `${process.env.REACT_APP_API_URL}/projects/${memberId}/${projectId}/files/${file.file_id}`
+          //   )
+          //   .then((response) => {
+          //     console.log(response);
+          //     if (matchProject) {
+          //       queryClient.invalidateQueries("project-files");
+          //     } else if (matchFinal) {
+          //       queryClient.invalidateQueries("final-files");
+          //     }
+          //   })
+          //   .catch((error) => {
+          //     console.error("Error:", error);
+          //   });
+        }
+      };
+
+
     return (
         <Link to={`/${project.projectId}/final-files`}>
             <div className="box">
@@ -22,7 +57,10 @@ const PjBox = ({ project }) => {
                 <div className="projectInfo">
                     <p className='h4'>{project.projectName}</p>
                     <br />
-                    <p className='p'>{project.projectStartDate}~{project.projectEndDate}</p>
+                    <p className='p'>{project.projectStartDate}~{project.projectEndDate}
+                    <Delete onClick={onDelete}><FiTrash2 size="19"/></Delete>
+                    </p>
+                    
                 </div>
             </div>
         </Link>
