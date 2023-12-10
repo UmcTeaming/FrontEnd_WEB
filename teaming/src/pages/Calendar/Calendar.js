828d0d68f6e5d605502617b5ab39a8352bd82011
SuaJeong-winter <sua.jeong.developer@gmail.com>
2023-12-11 01:49:59 +0900

    프로젝트 생성 후 세부 일정 추가 페이지 코드 설명 주석 추가 - 코드 변경은 없음

--- teaming/src/pages/Calendar/Calendar.js
+++ teaming/src/pages/Calendar/Calendar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Calendarcalendar } from '../../components/CalendarComponents/Calendarcalendar'
 // import { Calendarcalendar } from '../../components/CalendarComponents/Calendarcalendar'
 
+// ================================================
+// 프로젝트 생성 후 해당 프로젝트에 대한 세부 일정을 추가,삭제,변경 할 수 있는 가장 상위의 컴포넌트
 
 
 export const Calendar = () => {
